39d439d4a26bb2632290cc335e804b4a123b465e
fodon11 <francisco.odon11@gmail.com>
2018-05-05 17:55:56 -0400

    Added array of moves per type

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,5 @@
-let pokemon = [
+// Predefined Javamon to be picked from for use in battle
+let javamon = [
     {name: 'Bulbasaur',
         type: 'grass',
         sprite: 'src',},
@@ -44,5 +45,102 @@ let pokemon = [
     {name: 'Garchomp',
         type: 'dragon',
         sprite: 'src',},
-        
+]
+
+// Defines array for players to create their own Javamon
+let userJavamon= []
+
+// Defines moves to be chosen from according to type when Javamon are picked
+// pp is the amount of times a move may be used, ranges from 4-8 according to amount of power
+// Speed determines which move will go first in a turn. The higher the number, the quicker it is
+// Range for speed is from 1-5 
+let moveSet = [
+    {type: 'grass',
+        moves: [
+            {name: 'Razor Leaf', power: 55, pp: 8, speed: 5},
+            {name: 'Leaf Tornado', power: 65, pp: 8, speed: 5},
+            {name: 'Frenzy Plant', power: 120, pp: 5, speed: 2},
+            {name: 'Leaf Blade', power: 90, pp: 6, speed: 3},
+            {name: 'Leaf Storm', power: 110, pp: 5, speed: 2},
+            {name: 'Needle Arm', power: 60, pp: 8, speed: 5},
+            {name: 'Power Whip', power: 100, pp: 6, speed: 3},
+            {name: 'Solar Beam', power: 110, pp: 5, speed: 2},
+        ]},
+    {type: 'rock',
+        moves: [
+            {name: 'Rollout', power: 40, pp: 8, speed: 5},
+            {name: 'Smack Down', power: 55, pp: 8, speed: 5},
+            {name: 'Head Smash', power: 130, pp: 4, speed: 1},
+            {name: 'Rock Tomb', power: 70, pp: 7, speed: 4},
+            {name: 'Rock Slide', power: 80, pp: 7, speed: 4},
+            {name: 'Diamond Storm', power: 110, pp: 5, speed: 2},
+            {name: 'Rock Blast', power: 90, pp: 6, speed: 3},
+            {name: 'Ancient Power', power: 99, pp: 6, speed: 3},
+        ]},
+    {type: 'fire',
+        moves: [
+            {name: 'Blast Burn', power: 130, pp: 4, speed: 1},
+            {name: 'Ember', power: 55, pp: 8, speed: 5},
+            {name: 'Fire Blast', power: 110, pp: 5, speed: 2},
+            {name: 'Fire Punch', power: 80, pp: 7, speed: 4},
+            {name: 'Inferno', power: 110, pp: 5, speed: 2},
+            {name: 'Mystical Fire', power: 68, pp: 8, speed: 5},
+            {name: 'Flamethrower', power: 90, pp: 6, speed: 3},
+            {name: 'Incinerate', power: 70, pp: 7, speed: 4},
+        ]},
+    {type: 'water',
+        moves: [
+            {name: 'Surf', power: 90, pp: 5, speed: 3},
+            {name: 'Water Gun', power: 50, pp: 8, speed: 5},
+            {name: 'Waterfall', power: 80, pp: 7, speed: 4},
+            {name: 'Dive', power: 90, pp: 6, speed: 3},
+            {name: 'Hydro Pump', power: 120, pp: 5, speed: 2},
+            {name: 'Brine', power: 100, pp: 6, speed: 3},
+            {name: 'Hydro Cannon', power: 130, pp: 4, speed: 1},
+            {name: 'Origin Pule', power: 70, pp: 7, speed: 4},
+        ]},
+    {type: 'poison',
+        moves: [
+            {name: 'Poison Gas', power: 70, pp: 7, speed: 4},
+            {name: 'Poison Sting', power: 80, pp: 7, speed: 4},
+            {name: 'Sludge', power: 50, pp: 8, speed: 5},
+            {name: 'Poison Fang', power: 110, pp: 5, speed: 2},
+            {name: 'Acid', power: 90, pp: 6, speed: 3},
+            {name: 'Belch', power: 130, pp: 4, speed: 1},
+            {name: 'Acid Spray', power: 80, pp: 7, speed: 4},
+            {name: 'Venom Drench', power: 100, pp: 5, speed: 2},
+        ]},
+    {type: 'normal',
+        moves: [
+            {name: 'Uproar', power: 90, pp: 6, speed: 3},
+            {name: 'Take Down', power: 100, pp: 6, speed: 2},
+            {name: 'Tackle', power: 70, pp: 7, speed: 4},
+            {name: 'Strength', power: 80, pp: 6, speed: 4},
+            {name: 'Slash', power: 60, pp: 8, speed: 5},
+            {name: 'Mega Kick', power: 120, pp: 5, speed: 2},
+            {name: 'Mega Punch', power: 130, pp: 4, speed: 1},
+            {name: 'Fury Swipe', power: 70, pp: 7, speed: 4},
+        ]},
+    {type: 'psychic',
+        moves: [
+            {name: 'Zen Headbutt', power: 110, pp: 5, speed: 2},
+            {name: 'Psystrike', power: 90, pp: 6, speed: 3},
+            {name: 'Pyscho Cut', power: 70, pp: 7, speed: 4},
+            {name: 'Psybeam', power: 130, pp: 4, speed: 1},
+            {name: 'Extrasensory', power: 80, pp: 7, speed: 4},
+            {name: 'Confusion', power: 60, pp: 8, speed: 5},
+            {name: 'Dream Eater', power: 90, pp: 6, speed: 3},
+            {name: 'Future Sight', power: 120, pp: 5, speed: 2},
+        ]},
+    {type: 'dragon',
+        moves: [
+            {name: 'Dragon Breath', power: 70, pp: 7, speed: 4},
+            {name: 'Dragon Pulse', power: 90, pp: 6, speed: 3},
+            {name: 'Dragon Claw', power: 100, pp: 6, speed: 3},
+            {name: 'Dragon Tail', power: 60, pp: 8, speed: 5},
+            {name: 'Twister', power: 80, pp: 7, speed: 4},
+            {name: 'Roar of Time', power: 130, pp: 4, speed: 1},
+            {name: 'Outrage', power: 120, pp: 5, speed: 2},
+            {name: 'Draco Meteor', power: 75, pp: 7, speed: 4},
+        ]},
 ]
